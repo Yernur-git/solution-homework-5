25dacf4324efcd21a4ac579b556de321fc585f29
Yernur <erkeerke349@gmail.com>
2025-04-03 17:33:14 +0500

    highresimage implementation

--- Proxy/HighResImage.java
+++ Proxy/HighResImage.java
@@ -1,4 +1,23 @@
 package Proxy;
 
 public class HighResImage implements Image{
+    private String fileName;
+
+    public HighResImage(String fileName) {
+        this.fileName = fileName;
+        load();
+    }
+
+    private void load() {
+        System.out.println("loading high-resoultion image " + fileName);
+    }
+
+    public void display() {
+        System.out.println("displaying thumbnail " + fileName);
+    }
+
+    @Override
+    public void displayFullImage() {
+        System.out.println("displaying full-resoultion image " + fileName);
+    }
 }
