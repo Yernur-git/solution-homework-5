297bb1c3eb2d99abfe15eb84e607cfb028448ca6
Yernur <erkeerke349@gmail.com>
2025-04-04 11:16:35 +0500

    created main class to demonstrate proxy pattern

--- Proxy/ProxyDemo.java
+++ Proxy/ProxyDemo.java
@@ -1,4 +1,11 @@
 package Proxy;
 
 public class ProxyDemo {
+    public static void main(String[] args) {
+        Image image = new ImageProxy("building.jpg");
+
+        image.display();
+
+        image.displayFullImage();
+    }
 }
