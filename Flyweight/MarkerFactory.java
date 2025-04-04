f98a816c038fe5126fcd19676b6d3c6060bca5ee
Yernur <erkeerke349@gmail.com>
2025-04-04 21:40:40 +0500

    implemented markerfactory

--- Flyweight/MarkerFactory.java
+++ Flyweight/MarkerFactory.java
@@ -1,4 +1,13 @@
 package Flyweight;
 
-public class MarkerFactory implements Marker{
+import java.util.HashMap;
+import java.util.Map;
+
+public class MarkerFactory{
+    private static final Map<String, Marker> markers = new HashMap<>();
+
+    public static Marker getMarker(String type) {
+        markers.putIfAbsent(type, new MarkerStyle());
+        return markers.get(type);
+    }
 }
