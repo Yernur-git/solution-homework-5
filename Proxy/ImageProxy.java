23ae1cb0edb74bda55c1ea1d84f1cc543f2cb8f2
Yernur <erkeerke349@gmail.com>
2025-04-04 10:58:36 +0500

    imageproxy implementation

--- Proxy/ImageProxy.java
+++ Proxy/ImageProxy.java
@@ -1,4 +1,23 @@
 package Proxy;
 
 public class ImageProxy implements Image{
+    private String filename;
+    private HighResImage realImage;
+
+    public ImageProxy(String filename) {
+        this.filename = filename;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("displaying thumbnail " + filename);
+    }
+
+    @Override
+    public void displayFullImage() {
+        if (realImage == null) {
+            realImage = new HighResImage(filename);
+        }
+        realImage.displayFullImage();
+    }
 }
