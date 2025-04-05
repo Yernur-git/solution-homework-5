294cfd0d9db32613b63d3cb18d8f69bf5e949608
Yernur <erkeerke349@gmail.com>
2025-04-05 09:31:48 +0500

    created main class to demonstrate Flyweight pattern

--- Flyweight/FlyweightDemo.java
+++ Flyweight/FlyweightDemo.java
@@ -1,4 +1,18 @@
 package Flyweight;
 
 public class FlyweightDemo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            Marker marker = MarkerFactory.getMarker("hospital");
+            marker.display(randomX(), randomY());
+        }
+    }
+
+    private static int randomX() {
+        return (int) (Math.random() * 100);
+    }
+
+    private static int randomY() {
+        return (int) (Math.random() * 100);
+    }
 }
